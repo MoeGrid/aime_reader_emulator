7fe2753bb90031344af93e5c71021ab270ba6be8
MoeGrid <1065380934@qq.com>
2020-12-29 23:51:38 +0800

    Felica支持

--- AimeReaderGUI/MainForm.Designer.cs
+++ AimeReaderGUI/MainForm.Designer.cs
@@ -35,12 +35,17 @@
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.StartBtn = new System.Windows.Forms.Button();
-            this.SwipeBtn = new System.Windows.Forms.Button();
+            this.SwipeAimeBtn = new System.Windows.Forms.Button();
             this.P1Rad = new System.Windows.Forms.RadioButton();
             this.P2Rad = new System.Windows.Forms.RadioButton();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.CardNumCom = new System.Windows.Forms.ComboBox();
+            this.IDmInp = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.PMmInp = new System.Windows.Forms.TextBox();
+            this.SwipeFeliCaBtn = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
@@ -74,11 +79,11 @@
             // 
             // ActionLogInp
             // 
-            this.ActionLogInp.Location = new System.Drawing.Point(6, 20);
+            this.ActionLogInp.Location = new System.Drawing.Point(6, 23);
             this.ActionLogInp.Multiline = true;
             this.ActionLogInp.Name = "ActionLogInp";
             this.ActionLogInp.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.ActionLogInp.Size = new System.Drawing.Size(488, 474);
+            this.ActionLogInp.Size = new System.Drawing.Size(808, 474);
             this.ActionLogInp.TabIndex = 3;
             // 
             // groupBox1
@@ -86,7 +91,7 @@
             this.groupBox1.Controls.Add(this.ComLogInp);
             this.groupBox1.Location = new System.Drawing.Point(12, 66);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(500, 500);
+            this.groupBox1.Size = new System.Drawing.Size(500, 503);
             this.groupBox1.TabIndex = 4;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "串口日志";
@@ -96,7 +101,7 @@
             this.groupBox2.Controls.Add(this.ActionLogInp);
             this.groupBox2.Location = new System.Drawing.Point(518, 66);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(500, 500);
+            this.groupBox2.Size = new System.Drawing.Size(820, 503);
             this.groupBox2.TabIndex = 5;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "操作日志";
@@ -111,15 +116,15 @@
             this.StartBtn.UseVisualStyleBackColor = true;
             this.StartBtn.Click += new System.EventHandler(this.StartBtn_Click);
             // 
-            // SwipeBtn
+            // SwipeAimeBtn
             // 
-            this.SwipeBtn.Location = new System.Drawing.Point(614, 25);
-            this.SwipeBtn.Name = "SwipeBtn";
-            this.SwipeBtn.Size = new System.Drawing.Size(54, 20);
-            this.SwipeBtn.TabIndex = 7;
-            this.SwipeBtn.Text = "刷卡";
-            this.SwipeBtn.UseVisualStyleBackColor = true;
-            this.SwipeBtn.Click += new System.EventHandler(this.SwipeBtn_Click);
+            this.SwipeAimeBtn.Location = new System.Drawing.Point(614, 25);
+            this.SwipeAimeBtn.Name = "SwipeAimeBtn";
+            this.SwipeAimeBtn.Size = new System.Drawing.Size(66, 20);
+            this.SwipeAimeBtn.TabIndex = 7;
+            this.SwipeAimeBtn.Text = "刷Aime";
+            this.SwipeAimeBtn.UseVisualStyleBackColor = true;
+            this.SwipeAimeBtn.Click += new System.EventHandler(this.SwipeAimeBtn_Click);
             // 
             // P1Rad
             // 
@@ -172,17 +177,66 @@
             this.CardNumCom.Size = new System.Drawing.Size(155, 20);
             this.CardNumCom.TabIndex = 13;
             // 
+            // IDmInp
+            // 
+            this.IDmInp.Location = new System.Drawing.Point(736, 25);
+            this.IDmInp.Name = "IDmInp";
+            this.IDmInp.Size = new System.Drawing.Size(106, 21);
+            this.IDmInp.TabIndex = 14;
+            this.IDmInp.Text = "0102030405060708";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(701, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "IDm:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(857, 28);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 12);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "PMm:";
+            // 
+            // PMmInp
+            // 
+            this.PMmInp.Location = new System.Drawing.Point(892, 25);
+            this.PMmInp.Name = "PMmInp";
+            this.PMmInp.Size = new System.Drawing.Size(104, 21);
+            this.PMmInp.TabIndex = 17;
+            this.PMmInp.Text = "0807060504030201";
+            // 
+            // SwipeFeliCaBtn
+            // 
+            this.SwipeFeliCaBtn.Location = new System.Drawing.Point(1002, 25);
+            this.SwipeFeliCaBtn.Name = "SwipeFeliCaBtn";
+            this.SwipeFeliCaBtn.Size = new System.Drawing.Size(66, 20);
+            this.SwipeFeliCaBtn.TabIndex = 18;
+            this.SwipeFeliCaBtn.Text = "刷FeliCa";
+            this.SwipeFeliCaBtn.UseVisualStyleBackColor = true;
+            this.SwipeFeliCaBtn.Click += new System.EventHandler(this.SwipeFeliCaBtn_Click);
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1029, 574);
+            this.ClientSize = new System.Drawing.Size(1350, 581);
+            this.Controls.Add(this.SwipeFeliCaBtn);
+            this.Controls.Add(this.PMmInp);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.IDmInp);
             this.Controls.Add(this.CardNumCom);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.P2Rad);
             this.Controls.Add(this.P1Rad);
-            this.Controls.Add(this.SwipeBtn);
+            this.Controls.Add(this.SwipeAimeBtn);
             this.Controls.Add(this.StartBtn);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
@@ -212,12 +266,17 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Button StartBtn;
-        private System.Windows.Forms.Button SwipeBtn;
+        private System.Windows.Forms.Button SwipeAimeBtn;
         private System.Windows.Forms.RadioButton P1Rad;
         private System.Windows.Forms.RadioButton P2Rad;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.ComboBox CardNumCom;
+        private System.Windows.Forms.TextBox IDmInp;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox PMmInp;
+        private System.Windows.Forms.Button SwipeFeliCaBtn;
     }
 }
 
--- AimeReaderGUI/MainForm.cs
+++ AimeReaderGUI/MainForm.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.IO.Ports;
 using System.Linq;
 using System.Text;
@@ -14,15 +11,43 @@ namespace AimeReaderGUI
 {
     public partial class MainForm : Form
     {
+        private enum NfcCmd
+        {
+            SG_NFC_CMD_GET_FW_VERSION = 0x30,
+            SG_NFC_CMD_GET_HW_VERSION = 0x32,
+            SG_NFC_CMD_RADIO_ON = 0x40,
+            SG_NFC_CMD_RADIO_OFF = 0x41,
+            SG_NFC_CMD_POLL = 0x42,
+            SG_NFC_CMD_MIFARE_SELECT_TAG = 0x43,
+            SG_NFC_CMD_MIFARE_SET_KEY_BANA = 0x50,
+            SG_NFC_CMD_MIFARE_READ_BLOCK = 0x52,
+            SG_NFC_CMD_MIFARE_SET_KEY_AIME = 0x54,
+            SG_NFC_CMD_MIFARE_AUTHENTICATE = 0x55, /* guess based on time sent */
+            SG_NFC_CMD_RESET = 0x62,
+            SG_NFC_CMD_FELICA_ENCAP = 0x71,
+        }
+
+        private enum FeliCaCmd
+        {
+            FELICA_CMD_POLL = 0x00,
+            FELICA_CMD_GET_SYSTEM_CODE = 0x0c,
+            FELICA_CMD_NDA_A4 = 0xa4,
+        }
+
+        private class SwipeObj
+        {
+            public string Card = "";
+            public string IDm = "";
+            public string PMm = "";
+            public int Type = 0; // 0 无卡 1 Aime 2 FeliCa
+        };
+
         private SerialPort Com;
         private Thread RecvThread;
         private Thread PkgHandleThread;
         private Queue<byte> RecvQueue = new Queue<byte>();
 
-        private string Card = "";
-        private int Player = 0;
-        private bool Can = false;
-
+        private SwipeObj[] PlayerState = new SwipeObj[] { new SwipeObj(), new SwipeObj() };
 
         public MainForm()
         {
@@ -65,26 +90,58 @@ namespace AimeReaderGUI
             }
         }
 
-        private void SwipeBtn_Click(object sender, EventArgs e)
+        private void SwipeAimeBtn_Click(object sender, EventArgs e)
         {
             if (CardNumCom.Text.Length != 20)
             {
-                MessageBox.Show("卡号不正确", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Aime卡号不正确", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
-            SwipeBtn.Enabled = false;
-            Card = CardNumCom.Text;
-            Player = P1Rad.Checked ? 1 : P2Rad.Checked ? 2 : 0;
-            Can = true;
+            SwipeAimeBtn.Enabled = false;
+
+            var p = PlayerState[P1Rad.Checked ? 0 : P2Rad.Checked ? 1 : 3];
+            p.Card = CardNumCom.Text;
+            p.Type = 1;
+
             new System.Threading.Timer(new TimerCallback((obj) =>
             {
                 Action action = () =>
                 {
-                    SwipeBtn.Enabled = true;
-                    Can = false;
+                    SwipeAimeBtn.Enabled = true;
+                    p.Type = 0;
                 };
                 Invoke(action);
-            }), this, 5000, 0);
+            }), this, 3000, 0);
+        }
+
+        private void SwipeFeliCaBtn_Click(object sender, EventArgs e)
+        {
+            if (IDmInp.Text.Length != 16)
+            {
+                MessageBox.Show("IDm不正确", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (PMmInp.Text.Length != 16)
+            {
+                MessageBox.Show("PMm不正确", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            SwipeFeliCaBtn.Enabled = false;
+
+            var p = PlayerState[P1Rad.Checked ? 0 : P2Rad.Checked ? 1 : 3];
+            p.IDm = IDmInp.Text;
+            p.PMm = PMmInp.Text;
+            p.Type = 2;
+
+            new System.Threading.Timer(new TimerCallback((obj) =>
+            {
+                Action action = () =>
+                {
+                    SwipeFeliCaBtn.Enabled = true;
+                    p.Type = 0;
+                };
+                Invoke(action);
+            }), this, 3000, 0);
         }
 
         private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
@@ -149,128 +206,252 @@ namespace AimeReaderGUI
                 // 数据
                 byte[] data = body.Skip(5).Take(body[4]).ToArray();
                 // 处理封包
-                GotPkg(addr, comm, data);
+                GotPkg(addr, (NfcCmd)comm, data);
             }
         }
 
-        private void GotPkg(byte addr, byte comm, byte[] data)
+        private void GotPkg(byte addr, NfcCmd comm, byte[] data)
         {
-            ActionLog(string.Format("地址: {0} 操作: {1} 数据: {2}", addr.ToString("X2"), comm.ToString("X2"), ToHex(data)));
+            ActionLog(string.Format("地址: {0} 操作: {1} 数据: {2}", addr.ToString("X2"), comm.ToString(), ToHex(data)));
 
-            // 未知. reset?
-            if (comm == 0x62)
+            // 重置
+            if (comm == NfcCmd.SG_NFC_CMD_RESET)
             {
-                SendPkg(addr, comm, null);
+                SendPkg(addr, comm, false, null);
             }
 
             // 获取固件版本
-            if (comm == 0x30)
+            if (comm == NfcCmd.SG_NFC_CMD_GET_FW_VERSION)
             {
-                SendPkg(addr, comm, Encoding.ASCII.GetBytes("TN32MSEC003S F/W Ver1.2E"));
+                SendPkg(addr, comm, false, Encoding.ASCII.GetBytes("TN32MSEC003S F/W Ver1.2E"));
             }
 
             // 获取硬件版本
-            if (comm == 0x32)
+            if (comm == NfcCmd.SG_NFC_CMD_GET_HW_VERSION)
             {
-                SendPkg(addr, comm, Encoding.ASCII.GetBytes("TN32MSEC003S H/W Ver3.0J"));
+                SendPkg(addr, comm, false, Encoding.ASCII.GetBytes("TN32MSEC003S H/W Ver3.0J"));
             }
 
             // 设置Mifare KeyA
             // 57 43 43 46 76 32
             // WCCFv2
-            if (comm == 0x54)
+            if (comm == NfcCmd.SG_NFC_CMD_MIFARE_SET_KEY_AIME)
             {
-                SendPkg(addr, comm, null);
+                SendPkg(addr, comm, false, null);
             }
 
             // 设置Mifare KeyB
             // 60 90 D0 06 32 F5
-            if (comm == 0x50)
+            if (comm == NfcCmd.SG_NFC_CMD_MIFARE_SET_KEY_BANA)
             {
-                SendPkg(addr, comm, null);
+                SendPkg(addr, comm, false, null);
             }
 
-            // 检查读卡器连接?
+            // 广播开启
             // 03
-            if (comm == 0x40)
+            if (comm == NfcCmd.SG_NFC_CMD_RADIO_ON)
             {
-                SendPkg(addr, comm, null);
+                SendPkg(addr, comm, false, null);
             }
 
-            // 检查读卡器连接?
-            if (comm == 0x41)
+            // 广播关闭
+            if (comm == NfcCmd.SG_NFC_CMD_RADIO_OFF)
             {
-                SendPkg(addr, comm, null);
+                SendPkg(addr, comm, false, null);
             }
 
-            // 检卡
-            if (comm == 0x42)
+            // 检测卡
+            if (comm == NfcCmd.SG_NFC_CMD_POLL)
             {
-                if(Can && addr + 1 == Player)
+                var p = PlayerState[addr];
+                if (p.Type == 1)
                 {
-                    // 有卡
-                    SendPkg(addr, comm, new byte[] { 0x01, 0x10, 0x04, 0x70, 0x0A, 0xE2, 0xBF });
-                    Can = false;
+                    // Aime
+                    var tmp = new byte[] {
+                        // uint8_t count
+                        0x01,
+                        // uint8_t type
+                        0x10,
+                        // uint8_t id_len
+                        0x04,
+                        // uint32_t uid
+                        0x01, 0x02, 0x03, 0x04,
+                    };
+                    SendPkg(addr, comm, false, tmp);
+                    p.Type = 0;
+                }
+                else if (p.Type == 2)
+                {
+                    // FeliCa
+                    var tmp = new byte[] {
+                        // uint8_t count
+                        0x01,
+                        // uint8_t type
+                        0x20,
+                        // uint8_t id_len
+                        0x0F,
+                        // uint64_t IDm
+                        0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+                        // uint64_t PMm;
+                        0x08, 0x07, 0x06, 0x05, 0x04, 0x03, 0x02, 0x01,
+                    };
+                    SendPkg(addr, comm, false, tmp);
+                    p.Type = 0;
                 }
                 else
                 {
                     // 无卡
-                    SendPkg(addr, comm, new byte[] { 0x00 });
+                    SendPkg(addr, comm, false, new byte[] { 0x00 });
                 }
             }
 
-            // 防冲撞?
+            // 按UID选择MiFare?
             // xx xx xx xx (四字节Mifare UID)
-            if (comm == 0x43)
+            if (comm == NfcCmd.SG_NFC_CMD_MIFARE_SELECT_TAG)
             {
-                SendPkg(addr, comm, null);
+                SendPkg(addr, comm, false, null);
             }
 
-            // 退卡?
-            // xx xx xx xx (四字节Mifare UID)
-            // if (comm == 0x44)
-            // {
-            //     SendPkg(addr, comm, null);
-            // }
-
-            // 选卡?
+            // Mifare卡认证
             // xx xx xx xx 03 (四字节Mifare UID) 
-            if (comm == 0x55)
+            if (comm == NfcCmd.SG_NFC_CMD_MIFARE_AUTHENTICATE)
             {
-                SendPkg(addr, comm, null);
+                SendPkg(addr, comm, false, null);
             }
 
-            // 读卡
-            // 可能从Mifare扇区0读取块1和2.
+            // 从Mifare扇区0读取块1和2.
             // 块0包含"供应商信息"和UID.
             // 块1的内容未知, 可能是AiMe数据库信息.
-            // 块2的最后10个字节(十六进制)印在卡上("本地唯一ID").
+            // 块2的最后10个字节(十六进制)印在卡上("本地唯一ID")
             // (第3块包含加密密钥, 因此不允许读取)
-            if (comm == 0x52)
+            if (comm == NfcCmd.SG_NFC_CMD_MIFARE_READ_BLOCK)
             {
                 // Console.WriteLine("读取扇区: " + data[4]);
-                if (data[4] == 1)
+                var p = PlayerState[addr];
+                var block = data[4];
+                if (block == 1)
                 {
-                    SendPkg(addr, comm, new byte[] {
-                        0x00, 0x10,
+                    SendPkg(addr, comm, false, new byte[] {
                         0x53, 0x42, 0x53, 0x44, 0x00, 0x00, 0x00, 0x00,
                         0x00, 0x00, 0x00, 0x00, 0x00, 0x4F, 0x3D, 0x46
                     });
                 }
-                else if (data[4] == 2)
+                else if (block == 2)
                 {
                     var tmp = new byte[] {
-                        0x00, 0x10,
                         0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x11, 0x11,
                         0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11
                     };
-                    HexToByte(Card).CopyTo(tmp, 8);
-                    SendPkg(addr, comm, tmp);
+                    HexToByte(p.Card).CopyTo(tmp, 6);
+                    SendPkg(addr, comm, false, tmp);
+                }
+            }
+
+            // Felica处理
+            if (comm == NfcCmd.SG_NFC_CMD_FELICA_ENCAP)
+            {
+                // === pub req ===
+                // 01 02 03 04 05 06 07 08  IDm
+                // 06                       Encap payload length
+                // 00                       FeliCa cmd
+                var idm = data.Take(8).ToArray();
+                var len = data.Skip(8).First();
+                var cmd = (FeliCaCmd)data.Skip(9).First();
+
+                if (cmd == FeliCaCmd.FELICA_CMD_POLL)
+                {
+                    // === req ===
+                    // FF FF                    System code
+                    // 01                       Request code
+                    // 0F                       Time slot
+                    var systemCode = data.Skip(10).Take(2).ToArray();
+                    var requestCode = data.Skip(12).First();
+                    var timeSlot = data.Skip(13).First();
+
+                    Console.WriteLine(string.Format(
+                        "IDm: {0} Len: {1} Cmd: {2} SystemCode: {3} RequestCode: {4} TimeSlot: {5}",
+                        ToHex(idm),
+                        len.ToString("X2"),
+                        cmd.ToString(),
+                        ToHex(systemCode),
+                        requestCode.ToString("X2"),
+                        timeSlot.ToString("X2")
+                    ));
+
+                    // === res ===
+                    // 02                       cmd + 1
+                    // 01 02 03 04 05 06 07 08  IDm
+                    // 08 07 06 05 04 03 02 01  PMm
+                    // 88 99                    Request code == 0x01 ? System code
+                    if (requestCode == 0x01)
+                    {
+                        var tmp = new byte[] {
+                            0x00,
+                            0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+                            0x08, 0x07, 0x06, 0x05, 0x04, 0x03, 0x02, 0x01,
+                            0x88, 0x99
+                        };
+                        tmp[0] = (byte)(cmd + 1);
+                        SendPkg(addr, comm, false, tmp);
+                    }
+                }
+                else if (cmd == FeliCaCmd.FELICA_CMD_GET_SYSTEM_CODE)
+                {
+                    // === req ===
+                    // 01 02 03 04 05 06 07 08  IDm2
+                    var idm2 = data.Skip(10).Take(8).ToArray();
+                    Console.WriteLine(string.Format(
+                        "IDm: {0} Len: {1} Cmd: {2} IDm2: {3}",
+                        ToHex(idm),
+                        len.ToString("X2"),
+                        cmd.ToString(),
+                        ToHex(idm2)
+                    ));
+                    // === res ===
+                    // 02                       cmd + 1
+                    // 01 02 03 04 05 06 07 08  IDm
+                    // 01                       Number of system codes
+                    // 88 99                    System code
+                    var tmp = new byte[] {
+                        0x00,
+                        0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+                        0x01,
+                        0x88, 0x99
+                    };
+                    tmp[0] = (byte)(cmd + 1);
+                    SendPkg(addr, comm, false, tmp);
+                }
+                else if (cmd == FeliCaCmd.FELICA_CMD_NDA_A4)
+                {
+                    // === req ===
+                    // 01 02 03 04 05 06 07 08  IDm2
+                    // 00                       Unknow
+                    var idm2 = data.Skip(10).Take(8).ToArray();
+                    var unknow = data.Skip(18).First();
+                    Console.WriteLine(string.Format(
+                        "IDm: {0} Len: {1} Cmd: {2} IDm2: {3} Unknow: {4}",
+                        ToHex(idm),
+                        len.ToString("X2"),
+                        cmd.ToString(),
+                        ToHex(idm2),
+                        unknow.ToString("X2")
+                    ));
+                    // === res ===
+                    // 02                       cmd + 1
+                    // 01 02 03 04 05 06 07 08  IDm
+                    // 00                       Unknow
+                    var tmp = new byte[] {
+                        0x00,
+                        0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+                        0x00
+                    };
+                    tmp[0] = (byte)(cmd + 1);
+                    SendPkg(addr, comm, false, tmp);
                 }
             }
         }
 
-        private void SendPkg(byte addr, byte comm, byte[] data)
+        private void SendPkg(byte addr, NfcCmd comm, bool err, byte[] data)
         {
             byte dataLen = data == null ? (byte)0 : (byte)data.Length;
             byte allLen = (byte)(dataLen + 8);
@@ -287,9 +468,9 @@ namespace AimeReaderGUI
             // 序号
             buff[3] = 0x00;
             // 操作
-            buff[4] = comm;
+            buff[4] = (byte)comm;
             // 状态
-            buff[5] = 0x00;
+            buff[5] = (byte)(err ? 0x01 : 0x00);
             // 荷载长度
             buff[6] = dataLen;
             // 数据
@@ -301,7 +482,7 @@ namespace AimeReaderGUI
             // 发送
             Write(buff);
 
-            ActionLog(string.Format("地址: {0} 操作: {1} 数据: {2}", addr.ToString("X2"), comm.ToString("X2"), ToHex(data)), true);
+            ActionLog(string.Format("地址: {0} 操作: {1} 数据: {2}", addr.ToString("X2"), comm.ToString(), ToHex(data)), true);
         }
 
         private byte Read(bool escape = false)
@@ -402,5 +583,6 @@ namespace AimeReaderGUI
                 b[i] = Convert.ToByte(str.Substring(i * 2, 2), 16);
             return b;
         }
+
     }
 }
